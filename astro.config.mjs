2008e1d89190e9eea2b1e0b3215701d9b750c353
NCDinesh <deenez.npl1@gmail.com>
2025-01-11 17:19:50 +0545

    almost completed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import astroIcon from 'astro-icon';
 import image from "@astrojs/image"
+import sitemap from '@astrojs/sitemap';
 
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
+  site: 'https://astro-blog.netlify.app',
   integrations: [astroIcon({
     collections: 'all'
-  }), mdx(),image({serviceEntryPoint: '@astrojs/image/sharp'})]
+  }), mdx(),image({serviceEntryPoint: '@astrojs/image/sharp'}),sitemap()]
 });
